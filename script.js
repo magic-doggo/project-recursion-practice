23a65d095f89a99bfed78c07830d774ab9c6200f
magic-doggo <109861946+magic-doggo@users.noreply.github.com>
2024-05-12 13:11:20 +0300

    Write a function fibs which takes a number and returns an array containing that many numbers from the Fibonacci sequence

--- script.js
+++ script.js
@@ -1,10 +1,24 @@
-function fibonacci(n) {
-    if (n < 2) {
-        return n;
-    }
-    else {
-        return (fibonacci(n-1) + fibonacci(n-2))
-    }
-}
+// function fibonacci(n) {
+//     if (n < 2) {
+//         return n;
+//     }
+//     else {
+//         return (fibonacci(n-1) + fibonacci(n-2))
+//     }
+// }
 
-console.log(fibonacci(7))
+function fibs(n) {
+    if (n < 0) return "error";
+    if (n == 0) return 0;
+    let firstNum = 1;
+    let secondNum = 0;
+    let fibonacciArray = [0];
+    for (let i = 2; i <= n; i++) {
+        let current = firstNum + secondNum;
+        fibonacciArray.push(current)
+        firstNum = secondNum;
+        secondNum = current;
+    }
+    return fibonacciArray;
+}
+console.log(fibs(8))
